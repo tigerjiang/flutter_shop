c8a9e66a5c5afad326858d12703c49c1d598d6cf
jiangzehu <jiangzehu@xiaomi.com>
2020-03-29 23:10:21 +0800

    improvement : 删除kotlin的加载

---
+++ lib/config/index.dart
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 29/3/2020
+ *
+ * Created by Tiger
+ *
+ * mail : jzh2012@163.com
+ */
+
+export 'color.dart';
+export 'font.dart';
+export 'http_conf.dart';
+export 'length.dart';
+export 'string.dart';
