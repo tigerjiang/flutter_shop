0de912db1471c96a634b7730a809abfdb352b290
jiangzehu <jiangzehu@xiaomi.com>
2020-04-09 22:51:57 +0800

    [feature] 添加顶部轮播图

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shop/config/string.dart';
 import '../service/http_service.dart';
 import 'dart:convert';
@@ -16,24 +17,21 @@ import '../config/index.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
-class HomePage extends StatefulWidget{
-
-  _HomePageState createState()=> _HomePageState();
-
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
 }
 
-
-class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
-
+class _HomePageState extends State<HomePage>
+    with AutomaticKeepAliveClientMixin {
   //防止刷新处理，保持当前状态
   @override
   bool get wantKeepAlive => true;
 
-
-  GlobalKey<RefreshFooterState> _footerKey = new GlobalKey<RefreshFooterState>();
+  GlobalKey<RefreshFooterState> _footerKey =
+      new GlobalKey<RefreshFooterState>();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     print('首页刷新了...');
   }
@@ -41,20 +39,22 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(244,245,245,1.0),
-      appBar: AppBar(title: Text(KString.homeTitle),),
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      appBar: AppBar(
+        title: Text(KString.homeTitle),
+      ),
       body: FutureBuilder(
         future: request('homePageContext', formData: null),
-        builder: (context,snapshot){
-          if(snapshot.hasData){
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
             var data = json.decode(snapshot.data.toString());
-            List<Map> swipDataList = (data['data']['slides'] as List).cast(); //轮播图
+            List<Map> swiperDataList =
+                (data['data']['slides'] as List).cast(); //轮播图
             List<Map> category = (data['data']['category'] as List).cast(); //分类
-            List<Map> recommendList = (data['data']['recommend'] as List).cast(); //商品推荐
-            List<Map> floor1 = (data['data']['floor1'] as List).cast();//底部商品推荐
+            List<Map> recommendList =
+                (data['data']['recommend'] as List).cast(); //商品推荐
+            List<Map> floor1 = (data['data']['floor1'] as List).cast(); //底部商品推荐
             Map fp1 = data['data']['floor1Pic']; //广告
-
-
 
             return EasyRefresh(
               refreshFooter: ClassicsFooter(
@@ -69,14 +69,14 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
               ),
               child: ListView(
                 children: <Widget>[
+                  SwiperDiy(swiperDataList: swiperDataList,)
                 ],
               ),
               loadMore: () async {
                 print('开始加载更多');
               },
             );
-
-          }else{
+          } else {
             return Center(
               child: Text('加载中...'),
             );
@@ -85,9 +85,38 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
       ),
     );
   }
+}
 
 //首页轮播组件编写
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
 
+  SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
 
-
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    return Container(
+      color: Colors.white,
+      height: ScreenUtil.instance.setHeight(333),
+      width: ScreenUtil.instance.setWidth(750),
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+              onTap: () {
+              },
+              child: Image.network(
+                "${swiperDataList[index]['image']}",
+                fit: BoxFit.cover,
+              ));
+        },
+        //图片数量
+        itemCount: swiperDataList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
+  }
 }
+
+
